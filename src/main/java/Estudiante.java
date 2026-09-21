117c648e0ab53d346afbbded7d8beff9feaa4fbd
cristian12901 <cristian_ale.ramirez@uao.edu.co>
2026-09-21 11:11:59 -0500

    cambio 3

---
+++ src/main/java/Estudiante.java
@@ -0,0 +1,12 @@
+public class Estudiante {
+    String codigo, nombre, carrera;
+    double notaDesarrollo, notaDefinitiva;
+
+    public Estudiante(String codigo, String nombre, String carrera, double notaDesarrollo, double notaDefinitiva) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.carrera = carrera;
+        this.notaDesarrollo = notaDesarrollo;
+        this.notaDefinitiva = notaDefinitiva;
+    }
+}
